4ac83c1284ccdf01305b00fd4b3d818c4da1dcdf
whale0928 <rlagusrl928@gmail.com>
2024-08-01 01:51:30 +0900

    feat: add android webview internet settings

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:bottle_note_app/screen/web_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  //Flutter Framework will run the app
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
