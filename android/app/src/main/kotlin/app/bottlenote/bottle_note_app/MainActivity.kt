90638cc7e84ae00c0499b49d4fec15a0a67f0be7
hyejj19@naver.com <hyejj19@naver.com>
2024-09-05 22:45:54 +0900

    [Add] android MethodChannel for intent uri

--- android/app/src/main/kotlin/app/bottlenote/bottle_note_app/MainActivity.kt
+++ android/app/src/main/kotlin/app/bottlenote/bottle_note_app/MainActivity.kt
@@ -1,5 +1,38 @@
 package app.bottlenote.bottle_note_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+       private val CHANNEL = "intent.channel"
+
+        override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "handleIntentURI") {
+                val url = call.argument<String>("url")
+                handleIntentUri(url)
+                result.success(null)
+            }
+        }
+    }
+
+        private fun handleIntentUri(url: String?) {
+        if (url == null) return
+
+        try {
+            val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                val fallbackUrl = intent.getStringExtra("browser_fallback_url")
+                if (fallbackUrl != null) {
+                    // Fallback URL 처리
+                }
+            }
+        } catch (e: URISyntaxException) {
+            Log.e("MainActivity", "Invalid intent request", e)
+        }
+    }
+}
