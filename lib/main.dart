e0c37787708ac6f4e581596ed50de452f4a0f1a0
hgkim <hgkim@openrd.com>
2024-10-17 15:11:09 +0900

    feat: add fcm dependency for flutter ^3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:bottle_note_app/web_view/web_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  // Flutter 엔진과 관련된 바인딩을 초기화합니다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase를 초기화합니다.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseMessaging.instance.requestPermission(
+    badge: true, // 앱 아이콘에 배지를 표시할지 여부를 설정합니다.
+    alert: true, // 알림을 표시할지 여부를 설정합니다.
+    sound: true, // 알림 소리를 재생할지 여부를 설정합니다.
+  );
+
+  String? _fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM Token: $_fcmToken');
+  print('FCM Token: $_fcmToken');
+  print('FCM Token: $_fcmToken');
+
+  // 앱을 실행합니다.
   runApp(
     const MaterialApp(
       home: BottleNoteWebView(),
